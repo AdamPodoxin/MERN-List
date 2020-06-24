41e63abd0f39c333c69e80596e3fac630f36b8f7
Adam Podoxin <adam.podoxin@gmail.com>
2020-06-24 16:06:35 -0700

    Create AppNavbar component

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -11,7 +11,37 @@ import {
 } from 'reactstrap';
 
 class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    };
 
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="sm" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="/">MERN List</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="https://github.com/AdamPodoxin/MERN-List" target="_blank">GitHub</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
 }
+
+
 
 export default AppNavbar;
