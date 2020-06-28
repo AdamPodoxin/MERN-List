3a553499a7d0151cf28be33ffa274af4ca1ef58f
Adam Podoxin <adam.podoxin@gmail.com>
2020-06-27 18:13:57 -0700

    Prepare project for deployment

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = requre("path");
 
-const items = require('./routes/api/items');
+const items = require("./routes/api/items");
 
 const app = express();
 
@@ -10,16 +11,26 @@ const app = express();
 app.use(bodyParser.json());
 
 //DB config
-const db = require('./config/keys.js').mongoURI;
+const db = require("./config/keys.js").mongoURI;
 
 //Connect to MongoDB
 mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error(err));
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 //Use routes
-app.use('/api/items', items);
+app.use("/api/items", items);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
